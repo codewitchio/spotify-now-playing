11b650ffe7e886faaf4346c069e5f3e0461e2fe6
Aryuko <pachimaryu@gmail.com>
2020-05-22 15:14:30 +0200

    Add automatic token refreshing

--- src/authentication.js
+++ src/authentication.js
@@ -67,6 +67,28 @@ module.exports = {
             }
           });
         }
+    },
+    refresh_token: (req, res) => {
+        // requesting access token from refresh token
+        var refresh_token = req.query.refresh_token;
+        var authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            headers: { 'Authorization': 'Basic ' + (new Buffer(config.spotify.client_id + ':' + config.spotify.client_secret).toString('base64')) },
+            form: {
+                grant_type: 'refresh_token',
+                refresh_token: refresh_token
+            },
+            json: true
+        };
+
+        request.post(authOptions, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var access_token = body.access_token;
+                res.send({
+                    'access_token': access_token
+                });
+            }
+        });
     }
 }
 
--- src/public/script/spotify.js
+++ src/public/script/spotify.js
@@ -1,52 +1,82 @@
-$(function () {
-    /**
-     * Obtains parameters from the hash of the URL
-     * @return Object
-     */
-    function getHashParams() {
-      let hashParams = {};
-      let e, r = /([^&;=]+)=?([^&;]*)/g,
-          q = window.location.hash.substring(1);
-      while ( e = r.exec(q)) {
-         hashParams[e[1]] = decodeURIComponent(e[2]);
-      }
-      return hashParams;
-    }
-    
-    let params = getHashParams();
-    let access_token = params.access_token,
-        refresh_token = params.refresh_token,
-        error = params.error;
+config = {
+    interval: 3000
+}
 
+$(function () {    
+    let params = getHashParams()
+    let access_token = params.access_token
+    let refresh_token = params.refresh_token
+    let error = params.error
+
+    let interval
+    
     if (error) {
         alert('Authentication failed')
     } else {
-        if (access_token) {
-            console.log('access token is set')
-            
-            $.ajax({
-                url: 'https://api.spotify.com/v1/me/player/currently-playing',
-                headers: {
-                  'Authorization': 'Bearer ' + access_token
-                },
-                success: function(response) {
-                  console.log(response)
-
-                  imgUrl = response.item.album.images[1].url
-                  trackName = response.item.name
-                  artistName = response.item.artists[0].name
-
-                  console.log(imgUrl)
-                  $('#album-art').attr('src', imgUrl)
-                  $('#track-name').text(trackName)
-                  $('#artist-name').text(artistName)
-                }
-            });
+        if (access_token || refresh_token) {
+            getNowPlaying()
+            interval = setInterval(getNowPlaying, config.interval);
         }
+    }
+    
+    function getNowPlaying() {
+        $.ajax({
+            url: 'https://api.spotify.com/v1/me/player/currently-playing',
+            headers: { 'Authorization': 'Bearer ' + access_token },
+            success: (response) => updateUI(response),
+            error: (jqXHR) => { if(jqXHR.status == 401) { refreshToken() } }
+        })
+    }
+    
+    function updateUI(response) {
+        console.log(response)
+        
+        let isPlaying = response.is_playing
+        $('#title').text(isPlaying ? 'Now playing' : 'Paused')
+        if (isPlaying) {
+            $('#album-icon').addClass('rotating')
+        } else {
+            $('#album-icon').removeClass('rotating')
+        }
+        
+        let imgUrl = response.item.album.images[1].url
+        let trackName = response.item.name
+        let artistName = response.item.artists[0].name
+        
+        $('#album-art').attr('src', imgUrl)
+        $('#track-name').text(trackName)
+        $('#artist-name').text(artistName)
+    }
+    
+    function refreshToken() {
+        $.ajax({
+            url: '/refresh_token',
+            data: {
+                'refresh_token': refresh_token
+            }
+        }).done(function(data) {
+            access_token = data.access_token
+
+            getNowPlaying(access_token)
+        })
+    }
+    
+    /**
+    * Obtains parameters from the hash of the URL
+    * @return Object
+    */
+    function getHashParams() {
+        let hashParams = {}
+        let e, r = /([^&=]+)=?([^&]*)/g,
+        q = window.location.hash.substring(1)
+        while ( e = r.exec(q)) {
+            hashParams[e[1]] = decodeURIComponent(e[2])
+        }
+        return hashParams
     }
 
     // Can't get CORS to work with this
-    function getNowPlaying(access_token) {
+    function fetchNowPlaying(access_token) {
         url = 'https://api.spotify.com/v1/me/player/currently-playing'
         options = {
             mode: 'cors',
@@ -57,4 +87,4 @@ $(function () {
         }
         return fetch(url, options)
     }
-});
+})
