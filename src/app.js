e698ef26b55f029edd0b5e093b01ceb3da3ef927
Aryuko <pachimaryu@gmail.com>
2020-05-25 14:55:01 +0000

    use https instead of http to be able to host the app

--- src/app.js
+++ src/app.js
@@ -2,23 +2,32 @@
  * Project:     spotify-now-playing
  * Repository:  https://github.com/Aryuko/spotify-now-playing/
  * Author:      Aryuko
- * Version:     0.0.1
+ * Version:     0.0.2
 */
 
+const fs            = require('fs')
+const path          = require('path')
 const express       = require('express')
 const app           = express()
-const http          = require('http').createServer(app);
-const cookieParser  = require('cookie-parser');
-const cors          = require('cors');
+const https         = require('https')
+const cookieParser  = require('cookie-parser')
+const cors          = require('cors')
 
 const config        = require('./config.js')
 const auth          = require('./authentication.js')
 
-app.use(express.static(__dirname + '/public')).use(cookieParser()).use(cors())
+const publicPath = path.join(__dirname, '/public')
+app.use(express.static(publicPath)).use(cookieParser()).use(cors())
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/index.html'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/html/index.html')))
 app.get('/login', (req, res) => auth.login(req, res))
 app.get('/callback', (req, res) => auth.callback(req, res))
 app.get('/refresh_token', (req, res) => auth.refresh_token(req, res))
 
-http.listen(config.server.port, () => console.log('listening on', config.server.port))
+let privateKey = fs.readFileSync(config.server.privateKeyPath)
+let certificate = fs.readFileSync(config.server.certificatePath)
+
+https.createServer({
+    key: privateKey,
+    cert: certificate
+}, app).listen(config.server.port, () => console.log('listening on', config.server.port))
