d068e2ef01339bed662996d69e7517ff8e08c76c
tim hansen <timhansendev@gmail.com>
2020-05-26 14:31:00 -0400

    Add a directory for certificates. Update app.js to the new locations, use absolute path. Add tools directory. Add script for generating a rootca + client certs for the app.
    Signed-off-by: tim hansen <timhansendev@gmail.com>

--- src/app.js
+++ src/app.js
@@ -17,6 +17,8 @@ const config        = require('./config.js')
 const auth          = require('./authentication.js')
 
 const publicPath = path.join(__dirname, '/public')
+const certsPath = path.join(__dirname, '/certs')
+
 app.use(express.static(publicPath)).use(cookieParser()).use(cors())
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/html/index.html')))
@@ -25,8 +27,8 @@ app.get('/login', (req, res) => auth.login(req, res))
 app.get('/callback', (req, res) => auth.callback(req, res))
 app.get('/refresh_token', (req, res) => auth.refresh_token(req, res))
 
-let privateKey = fs.readFileSync(config.server.privateKeyPath)
-let certificate = fs.readFileSync(config.server.certificatePath)
+let privateKey = fs.readFileSync(path.join(certsPath, config.server.privateKeyPath))
+let certificate = fs.readFileSync(path.join(certsPath, config.server.certificatePath))
 
 https.createServer({
     key: privateKey,
---
+++ tools/createCA.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+#timh|2020|MIT license
+
+# Override by running with ENV vars set.
+rootdomain="${ROOT_DOMAIN:-domain}"
+clientdomain="${CLIENT_DOMAIN:-localhost}"
+rootca_key="${ROOT_CA_KEY:-rootca.key}"
+rootca_cert="${ROOT_CA_CERT:-rootca.crt}"
+client_key="${CLIENT_KEY:-$clientdomain.key}"
+client_cert="${CLIENT_KEY:-$clientdomain.crt}"
+client_csr="${CLIENT_CSR:-$clientdomain.csr}"
+keysize="${KEYSIZE:-4096}"
+expire="${EXPIRE:-365}"
+sha_bits="${SHA_BITS:-256}"
+
+# Root CA key
+createRootCA(){
+	openssl genrsa -out "$rootca_key" "$keysize"
+}
+
+# Create the Root CA Certificate.
+createRootCACert(){
+	openssl req -x509 -new -nodes -key "$rootca_key" -subj "/C=SE/ST=Stockholm/O=Dev/CN=$rootdomain" \
+		-addext "subjectAltName = IP:127.0.0.1" \
+		-sha"$sha_bits" -days "$expire" -out "$rootca_cert" 
+}
+
+# Create a certificate for development
+createClientKey(){
+	openssl genrsa -out "$client_key" 
+}
+
+# Create a CSR (Certificate Signing Request)
+createClientCSR(){
+	openssl req -new -sha256 -key "$client_key" -subj "/C=SE/ST=Stockholm/O=Dev/CN=$clientdomain" -out "$client_csr"
+}
+
+# Log out CSR to stdout
+verifyClientCSR(){
+	openssl req -in "$client_csr" -noout -text
+}
+
+# Accept your own CSR with your rootca key.
+acceptCSR(){
+	openssl x509 -req -in "$client_csr" \
+		-CA "$rootca_cert" -CAkey "$rootca_key" \
+		-CAcreateserial -out "$client_cert" \
+		-days "$expire" -sha"$sha_bits"
+}
+
+# Log certificate out to stdout
+verifyCert(){
+	openssl x509 -in "$client_cert" -text -noout
+}
+
+# Install into certs folder
+installRootCA(){
+	mkdir -p ../src/certs/
+	cp "$client_key" ../src/certs/
+	cp "$client_cert" ../src/certs/
+}
+
+# Print help for CLI
+printHelp() {
+	printf "\nbuild -b [yes|no]\nclean -c [yes|no]\n" 
+}
+
+# Clean up artifacts.
+cleanup(){
+	rm -rf ./*.key ./*.crt ./*.csr ./*.srl
+	rm -rf ../src/certs/*.key ../src/certs/*.crt
+}
+
+# Check if we are running as root
+if [ "$EUID" -ne 0 ]; then
+	printf "Please run this script as root in order to install the development certificate\n"
+	exit 1
+fi
+
+# Parse flags
+while getopts :h:b:c: flag
+do
+	case "${flag}" in
+		h) helper=${OPTARG};;
+		b) build=${OPTARG};;
+		c) clean=${OPTARG};;
+		*) printHelp && exit 1;;
+	esac
+done
+
+# Act on flags
+if [ "$helper" != "" ] || [ "$build" == "" ] && [ "$clean" == "" ]; then
+	printHelp
+	exit 0
+elif [ "$build" == "yes" ]; then
+	createRootCA
+	createRootCACert
+	createClientKey
+	createClientCSR
+	acceptCSR
+	verifyCert
+	printf "installing certs\n"
+elif [ "$clean" == "yes" ]; then
+	printf "cleaning...\n"
+	cleanup
+fi
+
+printf "exiting...\n"
+exit 0
